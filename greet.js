4b3d1d9f27b83e727b43c0dc7c5ba9e997feeaf2
Andrew Hampton <andrewwh@gmail.com>
2021-06-17 18:36:49 -0400

    move span to html

--- greet.js
+++ greet.js
@@ -5,12 +5,5 @@ import './greet.scss'
 let counter = 0
 
 export function greet() {
-  let greetingBox = document.querySelector('#greeting-box')
-  if (!greetingBox) {
-    greetingBox = document.createElement('div')
-    greetingBox.id = "greeting-box"
-    document.body.appendChild(greetingBox)
-  }
-
-  render(html`${counter++}: Heyo <img src="${cat}"></img>`, greetingBox)
+  render(html`${counter++}: Heyo <img src="${cat}"></img>`, document.querySelector('#greeting-box'))
 }
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('load', () => {
   const greetButton = document.querySelector('#greet')
   greetButton.addEventListener('click', () => {
