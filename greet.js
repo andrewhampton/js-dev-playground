a65de8e393650c00312eb7524b4d13e64e3f3a85
Andrew Hampton <andrewwh@gmail.com>
2021-06-17 17:34:45 -0400

    Add lit

--- greet.js
+++ greet.js
@@ -1,3 +1,14 @@
+import { html, render } from 'lit-html'
+
+let counter = 0
+
 export default function greet() {
-  alert('Hello world')
+  let greetingBox = document.querySelector('#greeting-box')
+  if (!greetingBox) {
+    greetingBox = document.createElement('div')
+    greetingBox.id = "greeting-box"
+    document.body.appendChild(greetingBox)
+  }
+
+  render(html`${counter++}: Heyo`, greetingBox)
 }
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import greet from './greet.js'
+
 window.addEventListener('load', () => {
   const greetButton = document.querySelector('#greet')
   greetButton.addEventListener('click', greet)
