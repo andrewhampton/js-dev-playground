0b8292c3acae27c09f0f2226f294e334c597c0ae
Andrew Hampton <andrewwh@gmail.com>
2021-06-19 15:38:13 -0400

    vite config

--- greet.js
+++ greet.js
@@ -1,9 +1,12 @@
-import { html, render } from 'lit-html'
-import cat from './cat.webp'
+import {html, render} from 'lit-html'
+import confetti from 'canvas-confetti'
+
 import './greet.scss'
+import cat from './cat.webp'
 
 let counter = 0
 
 export function greet() {
+  confetti()
   render(html`${counter++}: Heyo <img src="${cat}"></img>`, document.querySelector('#greeting-box'))
 }
